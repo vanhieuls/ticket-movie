109af981ceb6463d0d54c77b1639340db98b79de
vanhieuls <vanhieuls1325147@gmail.com>
2026-03-27 13:31:30 +0700

    update dns

--- src/main/java/com/example/english/Configuration/CommonConfig.java
+++ src/main/java/com/example/english/Configuration/CommonConfig.java
@@ -74,7 +74,7 @@ public class CommonConfig implements WebMvcConfigurer{
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
 
-                        .allowedOrigins("http://localhost:8080","http://localhost:3000","http://localhost:3001","http://localhost:5173","https://unrealistic-elton-denunciable.ngrok-free.dev")
+                        .allowedOrigins("https://ticket.vnhils.com","http://ticket.vnhils.com:8080","http://localhost:8080","http://localhost:3000","http://localhost:3001","http://localhost:5173","https://unrealistic-elton-denunciable.ngrok-free.dev")
                         .allowedMethods("PATCH","GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
